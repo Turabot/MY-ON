9e917835df993a7147631738c1b9fe8a4b412398
Artur <Turabot@i.ua>
2020-12-22 20:50:42 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/app'
+import './index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <App/>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
